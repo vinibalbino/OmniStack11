b30a73029610c2735212a322a4cff00b030f1393
silvavini9 <silvavinicius2018@gmail.com>
2020-03-29 23:56:14 -0300

    Funcionalidades como teste e verificações de entrada.

--- back-end/src/controllers/OngController.js
+++ back-end/src/controllers/OngController.js
@@ -1,5 +1,6 @@
+const generateUniqueId = require('../utils/generateUniqueId');
+
 const connection = require('../database/connection');
-const crypto = require('crypto');
 
 module.exports = {
   async index(request,response) {
@@ -8,8 +9,9 @@ module.exports = {
   },
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
-  
-    const id = crypto.randomBytes(4).toString('HEX')
+    
+    const id = generateUniqueId();
+    
     await connection('ongs').insert({
       id,
       name, 
